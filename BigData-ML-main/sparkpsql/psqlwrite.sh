15bb2df0cfa66cf1851ff10bf2742ca1b176c00a
MelissaGamboaNaranjoIBM <mgamboanaranjo@ibm.com>
2026-08-25 08:25:32 -0600

    Delete BigData-ML-main directory

--- BigData-ML-main/sparkpsql/psqlwrite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-spark-submit \
-  --driver-class-path postgresql-42.2.14.jar \
-  --jars postgresql-42.2.14.jar \
-  psqlwrite.py ../farms.csv ../productivity.csv ../agronomic_data.csv ../sensors_data.csv
-
-
